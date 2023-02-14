420207565f7b215fdeb9f2b3e346ec85aede54ce
Emre <kozanfurkanemre@gmail.com>
2023-02-14 14:52:01 +0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  mode: 'jit',
+  darkMode: 'class',
+  purge: [
+    './src/**/*.{js,ts,jsx,tsx}',
+    './src/components/**/*.{js,ts,jsx,tsx}',
+    './src/app/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    screens: {
+      xs: '320px',
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+      '2xl': '1536px',
+    },
+    extend: {
+
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [require('@tailwindcss/line-clamp'), require('@tailwindcss/forms')],
+};

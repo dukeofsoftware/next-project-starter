c4ccb973f9173646d25e097b508061e30b01fa59
dukeofsoftware <kozanfurkanemre@gmail.com>
2023-03-13 17:39:17 +0300

    fixed tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,6 @@
 module.exports = {
-  mode: 'jit',
-  darkMode: 'class',
-  purge: [
+
+  content: [
     './src/**/*.{js,ts,jsx,tsx}',
     './src/components/**/*.{js,ts,jsx,tsx}',
     './src/app/**/*.{js,ts,jsx,tsx}',
